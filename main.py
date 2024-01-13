82fc6e9464b76a0249267d7d3061017da2fe5471
pernebay-arailym <pernebayarailym@gmail.com>
2024-01-13 15:35:00 +0100

    draw rect

--- main.py
+++ main.py
@@ -17,6 +17,11 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
 def draw_grid(positions):
+    for position in positions:
+        column, row = position
+        top_left = (column*TILE_SIZE, row * TILE_SIZE)
+        pygame.draw.rect(screen, BLACK, (*top_left, TILE_SIZE, TILE_SIZE))        
+
     for row in range(GRID_HEIGHT):
         pygame.draw.line(screen, BLACK, (0, row * TILE_SIZE), (WIDTH, row * TILE_SIZE))
     for column in range(GRID_WIDTH):
@@ -26,6 +31,7 @@ def main():
     running = True
 
     positions = set()
+    positions.add((10, 10))
     while running:
         clock.tick(FPS)
 
