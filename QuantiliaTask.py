078c51fd214dcb4b9f7d9b458efec88fcb546ba0
pernebay-arailym <pernebayarailym@gmail.com>
2024-01-14 14:06:36 +0100

    :construction: create is_alive function

--- QuantiliaTask.py
+++ QuantiliaTask.py
@@ -1,15 +1,25 @@
 def count_life_around(grid, n, m, t):
-    count = 0
+    live_neighbors = 0
     for i in range(n-1, n+2):
         for j in range(m-1, m+2):
             if 0 <= i < len(grid) and 0 <= j < len(grid[0]) and (i != n or j != m):
-                count += grid[i][j][t]
-    return count
+                live_neighbors += grid[i][j][t]
+    return live_neighbors
+
+def is_alive(current_state, live_neighbors):
+        if current_state == 0:
+            return 1 if live_neighbors == 3 else 0
+        else:
+            return 0 if live_neighbors < 2 or live_neighbors > 3 else 1
 
 def evolve_with_boundary(state, steps):
+    rows = len(state)
+    cols = len(state [0])
     pass
 
 def evolve_without_boundary(state, steps):
+    rows = len(state)
+    cols = len(state[0])
     pass
 
 #usage:
